ee6b02de35838d69beca1d0bf77a7d1dac6d1521
Robert Schumann <dukfaar@gmail.com>
2018-07-13 11:39:30 +0200

    rename collection in item service to ease up copying

--- com/dukfaar/item/mgoItemService.go
+++ com/dukfaar/item/mgoItemService.go
@@ -6,27 +6,27 @@ import (
 )
 
 type MgoItemService struct {
-	db             *mgo.Database
-	itemCollection *mgo.Collection
+	db         *mgo.Database
+	collection *mgo.Collection
 }
 
 func NewMgoItemService(db *mgo.Database) *MgoItemService {
 	return &MgoItemService{
-		db:             db,
-		itemCollection: db.C("items"),
+		db:         db,
+		collection: db.C("items"),
 	}
 }
 
 func (s *MgoItemService) Create(model *Model) (*Model, error) {
 	model.ID = bson.NewObjectId()
 
-	err := s.itemCollection.Insert(model)
+	err := s.collection.Insert(model)
 
 	return model, err
 }
 
 func (s *MgoItemService) DeleteByID(id string) (string, error) {
-	err := s.itemCollection.RemoveId(bson.ObjectIdHex(id))
+	err := s.collection.RemoveId(bson.ObjectIdHex(id))
 
 	return id, err
 }
@@ -34,7 +34,7 @@ func (s *MgoItemService) DeleteByID(id string) (string, error) {
 func (s *MgoItemService) FindByID(id string) (*Model, error) {
 	var result Model
 
-	err := s.itemCollection.FindId(bson.ObjectIdHex(id)).One(&result)
+	err := s.collection.FindId(bson.ObjectIdHex(id)).One(&result)
 
 	return &result, err
 }
@@ -46,7 +46,7 @@ func (s *MgoItemService) HasElementBeforeID(id string) (bool, error) {
 		"$lt": bson.ObjectIdHex(id),
 	}
 
-	count, err := s.itemCollection.Find(query).Count()
+	count, err := s.collection.Find(query).Count()
 	return count > 0, err
 }
 
@@ -57,12 +57,12 @@ func (s *MgoItemService) HasElementAfterID(id string) (bool, error) {
 		"$gt": bson.ObjectIdHex(id),
 	}
 
-	count, err := s.itemCollection.Find(query).Count()
+	count, err := s.collection.Find(query).Count()
 	return count > 0, err
 }
 
 func (s *MgoItemService) Count() (int, error) {
-	count, err := s.itemCollection.Find(bson.M{}).Count()
+	count, err := s.collection.Find(bson.M{}).Count()
 	return count, err
 }
 
@@ -91,13 +91,13 @@ func (s *MgoItemService) List(first *int32, last *int32, before *string, after *
 	}
 
 	if last != nil {
-		count, _ := s.itemCollection.Find(query).Count()
+		count, _ := s.collection.Find(query).Count()
 
 		limit = int(*last)
 		skip = count - limit
 	}
 
 	var result []Model
-	err := s.itemCollection.Find(query).Skip(skip).Limit(limit).All(&result)
+	err := s.collection.Find(query).Skip(skip).Limit(limit).All(&result)
 	return result, err
 }
