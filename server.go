c01c850a5c84e0c7cee1491a9d04bd54c418882d
Robert Schumann <dukfaar@gmail.com>
2018-07-21 13:20:06 +0200

    make sure that client is logged in before sending a request

--- server.go
+++ server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/dukfaar/goUtils/env"
 	"github.com/dukfaar/goUtils/eventbus"
@@ -19,6 +22,62 @@ import (
 	graphql "github.com/graph-gophers/graphql-go"
 	graphqlRelay "github.com/graph-gophers/graphql-go/relay"
 )
+
+type ClientLoginHttpFetcher struct {
+	clientID             string
+	clientSecret         string
+	fetcher              *dukGraphql.HttpFetcher
+	accessToken          string
+	accessTokenExpiresAt time.Time
+}
+
+func NewClientLoginHttpFetcher(fetcher *dukGraphql.HttpFetcher, clientID string, clientSecret string) *ClientLoginHttpFetcher {
+	return &ClientLoginHttpFetcher{
+		fetcher:              fetcher,
+		clientID:             clientID,
+		clientSecret:         clientSecret,
+		accessTokenExpiresAt: time.Now(),
+	}
+}
+
+func (f *ClientLoginHttpFetcher) doLogin() error {
+	result, err := f.fetcher.Fetch(dukGraphql.Request{
+		Query: `query {
+			clientlogin(clientId: "` + f.clientID + `", clientSecret: "` + f.clientSecret + `") {
+				accessToken
+				accessTokenExpiresAt
+			}
+		}`,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	clientlogin := result.(map[string]interface{})
+	token := clientlogin["clientlogin"].(map[string]interface{})
+	f.accessToken = token["accessToken"].(string)
+	accessTokenExpiresAt, _ := strconv.ParseInt(token["accessTokenExpiresAt"].(string), 10, 64)
+	accessTokenExpiresAtSeconds := accessTokenExpiresAt / 1000
+	accessTokenExpiresAtNSeconds := (accessTokenExpiresAt % 1000) * 1e6
+
+	f.accessTokenExpiresAt = time.Unix(accessTokenExpiresAtSeconds, accessTokenExpiresAtNSeconds)
+
+	f.fetcher.SetHeader("Authentication", "Bearer "+f.accessToken)
+	return nil
+}
+
+func (f *ClientLoginHttpFetcher) Fetch(request dukGraphql.Request) (interface{}, error) {
+	now := time.Now()
+	if f.accessTokenExpiresAt.After(now) || f.accessTokenExpiresAt.Equal(now) {
+		err := f.doLogin()
+
+		if err != nil {
+			return nil, err
+		}
+	}
+	return f.fetcher.Fetch(request)
+}
 
 func main() {
 	dbSession, err := mgo.Dial(env.GetDefaultEnvVar("DB_HOST", "localhost"))
@@ -61,6 +120,35 @@ func main() {
 	}
 
 	nsqEventbus.Emit("service.up", serviceInfo)
+
+	apiGatewayFetcher, err := dukGraphql.NewHttpFetcher(
+		env.GetDefaultEnvVar("API_GATEWAY_HOST", "localhost")+":"+env.GetDefaultEnvVar("API_GATEWAY_PORT", "8090"),
+		env.GetDefaultEnvVar("API_GATEWAY_PATH", "/graphql"),
+	)
+
+	loginApiGatewayFetcher := NewClientLoginHttpFetcher(apiGatewayFetcher, "dukfaar-cloud-internal", "i am a ninja cat")
+
+	result, err := loginApiGatewayFetcher.Fetch(dukGraphql.Request{
+		Query: `query {
+			users {
+				edges {
+					node {
+						_id
+						roles {
+							edges {
+								node {
+									_id
+									name
+								}
+							}
+						}
+					}
+				}
+			}
+		}`,
+	})
+
+	fmt.Println(result)
 
 	nsqEventbus.On("service.up", "item", func(msg []byte) error {
 		newService := eventbus.ServiceInfo{}
