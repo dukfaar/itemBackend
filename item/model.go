934402e8387d95c768d675c179a83899c3d5aa56
Robert Schumann <robert.schumann@unic.com>
2018-10-18 11:08:19 +0200

    added types from rc

--- item/model.go
+++ item/model.go
@@ -1,23 +1,35 @@
 package item
 
 import (
+	"reflect"
+
+	"github.com/dukfaar/goUtils/graphql"
 	"github.com/dukfaar/goUtils/relay"
 	"github.com/globalsign/mgo/bson"
 )
 
-type Model struct {
-	ID          bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name        string        `json:"name,omitempty" bson:"name,omitempty"`
-	NamespaceID bson.ObjectId `json:"namespaceId,omitempty" bson:"namespaceId,omitempty"`
-	XivdbID     *int32        `json:"xivdbid,omitempty" bson:"xivdbid,omitempty"`
+type UnspoiledNodeTime struct {
+	Time           *int32  `json:"time,omitempty" bson:"time,omitempty" gql:"time"`
+	Duration       *int32  `json:"duration,omitempty" bson:"duration,omitempty" gql:"duration"`
+	AmPm           *string `json:"ampm,omitempty" bson:"ampm,omitempty" gql:"ampm"`
+	FolkloreNeeded *string `json:"folkloreNeeded,omitempty" bson:"folkloreNeeded,omitempty" gql:"folkloreNeeded"`
 }
 
-var GraphQLType = `
-type Item {
-	_id: ID
-	name: String
-	namespaceId: ID
-	xivdbid: Int
+type Model struct {
+	ID                bson.ObjectId      `json:"_id,omitempty" bson:"_id,omitempty" gql:"_id"`
+	Name              string             `json:"name,omitempty" bson:"name,omitempty" gql:"name"`
+	NamespaceID       bson.ObjectId      `json:"namespaceId,omitempty" bson:"namespaceId,omitempty" gql:"namespaceId"`
+	XivdbID           *int32             `json:"xivdbid,omitempty" bson:"xivdbid,omitempty" gql:"xivdbId"`
+	GatheringLevel    *int32             `json:"gatheringLevel,omitempty" bson:"gatheringLevel,omitempty" gql:"gatheringLevel"`
+	GatheringJob      *bson.ObjectId     `json:"gatheringJob,omitempty" bson:"gatheringJob,omitempty" gql:"gatheringJob"`
+	GatheringEffort   *int32             `json:"gatheringEffort,omitempty" bson:"gatheringEffort,omitempty" gql:"gatheringEffort"`
+	Price             *int32             `json:"price,omitempty" bson:"price,omitempty" gql:"price"`
+	PriceHQ           *int32             `json:"priceHQ,omitempty" bson:"priceHQ,omitempty" gql:"priceHq"`
+	UnspoiledNode     *bool              `json:"unspoiledNode,omitempty" bson:"unspoiledNode,omitempty" gql:"unspoiledNode"`
+	UnspoiledNodeTime *UnspoiledNodeTime `json:"unspoiledNodeTime,omitempty" bson:"unspoiledNodeTime,omitempty" gql:"unspoiledNodeTime"`
+	AvailableFromNpc  *bool              `json:"availableFromNpc,omitempty" bson:"availableFromNpc,omitempty" gql:"availableFromNpc"`
 }
-` +
+
+var GraphQLType = graphql.Build(reflect.TypeOf((*UnspoiledNodeTime)(nil)).Elem(), "UnspoiledNodeTime") +
+	graphql.Build(reflect.TypeOf((*Model)(nil)).Elem(), "Item") +
 	relay.GenerateConnectionTypes("Item")
--- item/resolver.go
+++ item/resolver.go
@@ -48,3 +48,116 @@ func (r *Resolver) NamespaceID(ctx context.Context) (*graphql.ID, error) {
 	id := graphql.ID(r.Model.NamespaceID.Hex())
 	return &id, nil
 }
+
+func (r *Resolver) GatheringLevel(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "Item.gatheringLevel.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.GatheringLevel, nil
+}
+
+func (r *Resolver) GatheringJob(ctx context.Context) (*graphql.ID, error) {
+	err := permission.Check(ctx, "Item.gatheringJob.read")
+	if err != nil {
+		return nil, err
+	}
+
+	id := graphql.ID(r.Model.GatheringJob.Hex())
+	return &id, nil
+}
+
+func (r *Resolver) GatheringEffort(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "Item.gatheringEffort.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.GatheringEffort, nil
+}
+
+func (r *Resolver) Price(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "Item.price.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.Price, nil
+}
+
+func (r *Resolver) PriceHQ(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "Item.priceHq.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.PriceHQ, nil
+}
+
+func (r *Resolver) UnspoiledNode(ctx context.Context) (*bool, error) {
+	err := permission.Check(ctx, "Item.unspoiledNode.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.UnspoiledNode, nil
+}
+
+func (r *Resolver) AvailableFromNpc(ctx context.Context) (*bool, error) {
+	err := permission.Check(ctx, "Item.availableFromNpc.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Model.AvailableFromNpc, nil
+}
+
+type UnspoiledNodeTimeResolver struct {
+	time *UnspoiledNodeTime
+}
+
+func (r *Resolver) UnspoiledNodeTime(ctx context.Context) (*UnspoiledNodeTimeResolver, error) {
+	err := permission.Check(ctx, "Item.unspoiledNodeTime.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return &UnspoiledNodeTimeResolver{r.Model.UnspoiledNodeTime}, nil
+}
+
+func (r *UnspoiledNodeTimeResolver) Time(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "UnspoiledNodeTime.time.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.time.Time, nil
+}
+
+func (r *UnspoiledNodeTimeResolver) Duration(ctx context.Context) (*int32, error) {
+	err := permission.Check(ctx, "UnspoiledNodeTime.duration.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.time.Duration, nil
+}
+
+func (r *UnspoiledNodeTimeResolver) AmPm(ctx context.Context) (*string, error) {
+	err := permission.Check(ctx, "UnspoiledNodeTime.ampm.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.time.AmPm, nil
+}
+
+func (r *UnspoiledNodeTimeResolver) FolkloreNeeded(ctx context.Context) (*string, error) {
+	err := permission.Check(ctx, "UnspoiledNodeTime.folkloreNeeded.read")
+	if err != nil {
+		return nil, err
+	}
+
+	return r.time.FolkloreNeeded, nil
+}
